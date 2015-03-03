84c6ceb032c704d1f26754807ca1529c51ab6e2c
seb <seb@ukr.net>
2015-03-03 17:26:57 +0200

    Added module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,53 @@
+use std::rc::Rc;
+use std::rc::Weak;
+use std::cell::RefCell;
 
-
-pub mod game
+pub mod backend
 {
-	#[allow(dead_code)]
-	pub struct Grig {
-	    map: Vec<i32>
+	#[allow(dead_code)] #[derive(Debug)]
+	pub enum Content {
+		Empty,
+		Bot,
+		Cargo,
+		Dock,
+		Checkpoint,
 	}
 
-	impl Grig {
-    	pub fn add(&mut self, value: i32){
+	#[allow(dead_code)] #[derive(Debug)]
+	pub struct GridCell 
+	{
+		pub id			: usize,
+		pub content 	: Content,
+		neighbors		: Vec<GridCell>,
+		
+	}
+	impl GridCell 
+	{
+        pub fn link(mut lhv : Box<GridCell>, mut rhv : Box<GridCell>) -> () 
+        {
+//            lhv.neighbors.push(rhv);
+//            rhv.neighbors.push(lhv);
+        }
+    }
+
+
+
+	#[allow(dead_code)] #[derive(Debug)]
+	pub struct Grig<T> {
+	    map: Vec<T>
+	}
+
+	impl<T> Grig<T> {
+        pub fn new() -> Grig<T> {
+            Grig {
+                map: vec![]
+            }
+        }
+
+    	pub fn add(&mut self, value: T) -> () {
         	self.map.push(value);
     	}
-    	pub fn print(&self){
-    		println!("map: {:?}", self.map);
-    	}
+
 	}
 }
 
