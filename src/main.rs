926d211447440d3b696a09ec0a96d6767fde5ad7
seb <seb@ukr.net>
2015-03-03 12:07:23 +0200

    Added module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+
+
+pub mod game
+{
+	#[allow(dead_code)]
+	pub struct Grig {
+	    map: Vec<i32>
+	}
+
+	impl Grig {
+    	pub fn add(&mut self, value: i32){
+        	self.map.push(value);
+    	}
+    	pub fn print(&self){
+    		println!("map: {:?}", self.map);
+    	}
+	}
+}
+
+
 fn main() {
     println!("Hello, world!");
+    //let a = Grid{};
 }
