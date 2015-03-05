ea6c0aeeeaf642f2c22bed5f3263487666d5b0a5
seb-odessa <seb@ukr.net>
2015-03-05 23:34:46 +0200

    GridCell refactored

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,68 @@
 #![feature(box_syntax)]
 #![feature(alloc)]
 
+use std::rc::Rc;
+
 
 mod backend {
 	use std::rc::Rc;
 	use std::rc::Weak;
 	use std::cell::RefCell;
 
-
-	type GridCellWrapper = RefCell<GridCell>;
-	type GridCellRc = Rc<GridCellWrapper>;
-	type GridCellLink = Option<Weak<GridCellWrapper>>;
-
-	#[allow(dead_code)] #[derive(Debug)]
-	pub struct GridCell
-	{
+	#[allow(dead_code)] 
+	#[derive(Debug)]	
+	pub struct GridCell	{
 		pub id			: usize,
-		pub link		: GridCellLink,
+		pub link		: RefCell<Option<Weak<GridCell>>>,
 	}
-
-	impl GridCell
-	{
+	impl GridCell {
 		#[allow(dead_code)]
-	    pub fn new(id:usize) -> GridCellRc {
-	    	Rc::new( RefCell::new ( GridCell { id : id,	link : None } ) ) 
-	    }
-
-		#[allow(dead_code)]
-	    pub fn print(&self) -> () {
-    		match self.link {
-    			Some(ref weak) => println!("DEMO id : {}, Some : {:?}", self.id, weak.upgrade().unwrap()),
-    			None => println!("DEMO id : {}, Some : None", self.id),
-    		};
+	    pub fn new(id:usize) -> GridCell {
+	    	GridCell { id : id,	link : RefCell::new(None) } 
 	    }
 
    		#[allow(dead_code)]
-	    pub fn link(lhv : GridCellRc, rhv : GridCellRc) -> () {
-	    	{
-	    		lhv.borrow_mut().link = Some(rhv.downgrade());
-	    	};
-	    	{
-	    		rhv.borrow_mut().link = Some(lhv.downgrade());
-	    	};
-	    }
+	    pub fn set_link(&self, rhv: Weak<GridCell>) -> () {
+            (*self.link.borrow_mut()) = Some(rhv);
+        }
 
+   		#[allow(dead_code)]
+	    pub fn get_link(&self) -> Option<Rc<GridCell>> {
+            match *self.link.borrow() {
+	    		Some(ref weak) => weak.upgrade(),
+	    		None => return None
+	    	}
+        }
 	}
 }
 
 fn main() {
-    let cell1 = backend::GridCell::new(1);    
-    let cell2 = backend::GridCell::new(2);    
-    let cell3 = backend::GridCell::new(3);
+    let cell1 = Rc::new(backend::GridCell::new(1));
+    let cell2 = Rc::new(backend::GridCell::new(2));
+    let cell3 = Rc::new(backend::GridCell::new(3));
         
-   	cell1.borrow().print();
-   	cell2.borrow().print();
-   	cell3.borrow().print();
+    {
+	  	println!("{:?}", cell1);
+  		println!("{:?}", cell2);
+	  	println!("{:?}", cell3);
+    }
+ 
+  	{
+  		cell1.set_link(cell2.downgrade());
+  		cell2.set_link(cell3.downgrade());
+  	}
 
-	cell1.borrow_mut().link = Some(cell2.downgrade());
-    cell2.borrow_mut().link = Some(cell3.downgrade());
-    
+    {
+	  	println!("{:?}", cell1);
+  		println!("{:?}", cell2);
+  		println!("{:?}", cell3);
+    } 	 
 
-   	cell1.borrow().print();
-   	cell2.borrow().print();
-   	cell3.borrow().print();
+	{
+	    println!("cell1.get_link() => {:?}", cell1.get_link());
+	    println!("cell2.get_link() => {:?}", cell2.get_link());
+	    println!("cell3.get_link() => {:?}", cell3.get_link());
+	}
+
+
  }
