e93f75e69065bb1be4ded48321d5284ea76d2b23
seb-odessa <seb@ukr.net>
2015-07-12 08:51:45 +0300

    Documentation added

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,3 +1,45 @@
+//! This library contains representation of the linked wetwork of cells.
+//!
+//! The possible way for traverse of the network
+//!
+//! For example:
+//!
+//! ```
+//! extern crate lib;
+//! 
+//! use lib::maploader::Map;
+//! use lib::gridcell::Neighbor;
+//! use lib::gameboard::GameBoard;
+//! use lib::gameboard::Command;
+//! 
+//! #[allow(dead_code)]
+//! fn main(){
+//!     // let map : Map = Map::load_map("maps/flat_map_3x3.json");
+//!     let map : Map = Map::demo_map();
+//!     let board : GameBoard = GameBoard::load(&map, 1, 9);
+//!
+//!     let route = vec![Neighbor::South,Neighbor::South,Neighbor::East,Neighbor::East,];
+//!     for direction in route.iter() {
+//!         println!("board.execute(Command::Move, {:?}) => {}", *direction, board.execute(Command::Move, *direction));
+//!         println!("board.is_complete() => {:?}", board.is_complete());
+//!     }
+//! }
+//! ```
+//!
+//! will produce:
+//!
+//! ```
+//! board.execute(Command::Move, South) => true
+//! board.is_complete() => false
+//! board.execute(Command::Move, South) => true
+//! board.is_complete() => false
+//! board.execute(Command::Move, East) => true
+//! board.is_complete() => false
+//! board.execute(Command::Move, East) => true
+//! board.is_complete() => true
+//! ```
+
+
 
 pub use self::gridcell::CellId;
 pub use self::gridcell::GridCell;
